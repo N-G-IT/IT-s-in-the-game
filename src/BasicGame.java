14b8d7a6bba6f20631174f58175bbd527d9ca15a
N-G-IT <573739@student.saxion.nl>
2024-11-22 09:55:31 +0100

    nieuw project toevoegen

--- src/BasicGame.java
+++ src/BasicGame.java
@@ -6,27 +6,44 @@ import nl.saxion.app.interaction.MouseEvent;
 
 public class BasicGame implements GameLoop {
 
+
+    int x,y;
+
+
     public static void main(String[] args) {
         SaxionApp.startGameLoop(new BasicGame(), 1000, 1000, 40);
     }
 
     @Override
     public void init() {
-
+        x = 500;
+        y = 500;
     }
 
     @Override
     public void loop() {
-
+        SaxionApp.drawCircle(x,y,100);
     }
 
     @Override
     public void keyboardEvent(KeyboardEvent keyboardEvent) {
 
+        if (keyboardEvent.isShiftDown()){
+            SaxionApp.clear();
+        }
+
+
+
     }
 
     @Override
     public void mouseEvent(MouseEvent mouseEvent) {
+        x = mouseEvent.getX();
+        y = mouseEvent.getY();
+
+
+
+
 
     }
 }
