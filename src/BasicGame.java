8aa9802639fc8875699c6b51d356695df400dd01
N-G-IT <573739@student.saxion.nl>
2024-11-26 08:53:02 +0100

    verbetering keuze menu

--- src/BasicGame.java
+++ src/BasicGame.java
@@ -7,12 +7,64 @@ import nl.saxion.app.interaction.MouseEvent;
 public class BasicGame implements GameLoop {
 
 
-    int x,y;
+
+
+
+
+
+
 
 
     public static void main(String[] args) {
         SaxionApp.startGameLoop(new BasicGame(), 1000, 1000, 40);
     }
+
+    int x,y;
+
+
+    public void doStuff() {
+        SaxionApp.printLine("Maka a choice:");
+        SaxionApp.printLine("0 is close the game");
+        SaxionApp.printLine("1 is the EN word list");
+        SaxionApp.printLine("2 is the Nl word list");
+        SaxionApp.print("Chose one of the above: ");
+
+        int choice = SaxionApp.readInt();
+
+
+        if (choice == 0) {
+
+            // close game
+
+
+        } else if (choice == 1) {
+
+            // kies engelse woordenlijst
+
+        } else if (choice == 2) {
+
+            // kies nederlandese woordenlijst
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     @Override
     public void init() {
