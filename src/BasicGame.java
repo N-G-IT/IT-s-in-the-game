48ac7cedc083c873003622a7707eeedae13f44f3
Niels <573739@student.saxion.nl>
2024-12-12 21:40:14 +0100

    metode galgje aangemaakt

--- src/BasicGame.java
+++ src/BasicGame.java
@@ -3,7 +3,6 @@ import nl.saxion.app.interaction.GameLoop;
 import nl.saxion.app.interaction.KeyboardEvent;
 import nl.saxion.app.interaction.MouseEvent;
 
-import java.text.BreakIterator;
 import java.util.ArrayList;
 
 public class BasicGame implements GameLoop {
@@ -23,7 +22,7 @@ public class BasicGame implements GameLoop {
 
     @Override
     public void loop() {
-        doStuff();
+        startscherm();
         if (!initialized) {
             init();
             initialized = true;
@@ -31,33 +30,41 @@ public class BasicGame implements GameLoop {
 
     }
 
-    public void doStuff() {
-        SaxionApp.drawText("Maak een keuze:",80,18,24);
-        SaxionApp.drawText("0 is sluit het spel",80,48,22);
-        SaxionApp.drawText("1 is de Engelse woordenlijst",80,78,22);
-        SaxionApp.drawText("2 is de Nederlandse woordenlijst",80,108,22);
-        SaxionApp.drawText("Kies een optie: ",80,138,22);
+    public void galgje() {
+
+    }
+
+    public void startscherm() {
+        SaxionApp.drawText("Maak een keuze:", 80, 18, 24);
+        SaxionApp.drawText("0 is sluit het spel", 80, 48, 22);
+        SaxionApp.drawText("1 is de Engelse woordenlijst", 80, 78, 22);
+        SaxionApp.drawText("2 is de Nederlandse woordenlijst", 80, 108, 22);
+        SaxionApp.drawText("Kies een optie: ", 80, 138, 22);
+
 
         int choice = SaxionApp.readInt();
         SaxionApp.drawText("Input received: " + choice, 80, 200, 22);
 
 
-        if (choice == 0) {
-            SaxionApp.drawText("Spel wordt gesloten.",80,168,22);
-            System. exit(0); // Sluit het programma
+        if (keyboardEvent(SaxionApp.KeyPressed('0'))) {
+            SaxionApp.drawText("Spel wordt gesloten.", 80, 168, 22);
+            System.exit(0); // Sluit het programma
         } else if (choice == 1) {
-            SaxionApp.drawText("Engelse woordenlijst wordt gekozen.",80,168,22);
+            SaxionApp.drawText("Engelse woordenlijst wordt gekozen.", 80, 168, 22);
         } else if (choice == 2) {
             String woord = woordenlijstnl();
-            SaxionApp.drawText("Gekozen woord: " + woord,80,168,22);
+            SaxionApp.drawText("Gekozen woord: " + woord, 80, 168, 22);
         } else {
-            SaxionApp.drawText("Ongeldige keuze. Probeer opnieuw.",80,168,22);
+            SaxionApp.drawText("Ongeldige keuze. Probeer opnieuw.", 80, 168, 22);
         }
     }
 
     @Override
     public void keyboardEvent(KeyboardEvent keyboardEvent) {
-        // Event-afhandeling voor toetsenbord
+        if (keyboardEvent.equals(1)) {
+
+
+        }
     }
 
     @Override
@@ -65,13 +72,18 @@ public class BasicGame implements GameLoop {
         // Event-afhandeling voor muis
     }
 
+
+
+
+
+
     // Nederlandse woordenlijst & random woord kiezen
     public String woordenlijstnl() {
         ArrayList<String> woorden = new ArrayList<>();
         woorden.add("Avontuur");
         woorden.add("Zandloper");
         woorden.add("Muzieknoot");
-        return woorden.get(SaxionApp.getRandomValueBetween(0, woorden.size()-1));
+        return woorden.get(SaxionApp.getRandomValueBetween(0, woorden.size() - 1));
     }
 
     // Engelse woordenlijst & random woord kiezen
@@ -80,7 +92,7 @@ public class BasicGame implements GameLoop {
         words.add("Apple");
         words.add("Car");
         words.add("House");
-        return words.get(SaxionApp.getRandomValueBetween(0, words.size()-1));
+        return words.get(SaxionApp.getRandomValueBetween(0, words.size() - 1));
     }
 }
 
